5a0887f74c61ed29581b73f32679e7eba015478a
rakib hasan <dev.rakibhasan1@gmail.com>
2025-03-16 04:01:14 +0600

    added active class and added loader

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ function scrollToLearn() {
     });
 };
 
+
+const showLoader = () => {
+    document.getElementById('loader').classList.remove('hidden');
+    document.getElementById('details-container').classList.add('hidden');
+};
+
+const hideLoader = () => {
+    document.getElementById('loader').classList.add('hidden');
+    document.getElementById('details-container').classList.remove('hidden');
+};
+
+
+const removerActiveClass = () => {
+    const activeButtons = document.getElementsByClassName('active');
+
+    for (let button of activeButtons) {
+        button.classList.remove('active');
+    }
+    console.log(activeButtons)
+};
+
+
 const loadButtons = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then(info => info.json())
@@ -26,7 +48,7 @@ const showbuttons = (categories) => {
 
         const div = document.createElement('div');
         div.innerHTML = `
-        <button onclick="loadCategories(${category.level_no})" class="btn text-[#422AD5] border-[#422AD5] hover:bg-[#422AD5] hover:text-white"><i class="fa-solid fa-book-open"></i>
+        <button id="btn-${category.level_no}" onclick="loadCategories(${category.level_no})" class="btn text-[#422AD5] border-[#422AD5] hover:bg-[#422AD5] hover:text-white"><i class="fa-solid fa-book-open"></i>
          Lesson-${category.level_no}</button>
         `
         buttonContainer.appendChild(div);
@@ -40,75 +62,61 @@ const lessonsDetails = (lessonId) => {
 };
 
 const showLessonsDetails = (lesson) => {
-    console.log(lesson);
     document.getElementById('lesson_details').showModal();
-    const detailsContainer = document.getElementById('details-container');
+    // const detailsContainer = document.getElementById('details-container');
 
-    document.getElementById('lesson-heading').innerText= `${lesson.word} ( ${lesson.pronunciation} )`;
-    document.getElementById('lesson-p1').innerText= 'Meaning';
-    document.getElementById('lesson-p2').innerText= lesson.meaning;
-    document.getElementById('lesson-p3').innerText= 'Example';
-    document.getElementById('lesson-p4').innerText= lesson.sentence;
-    document.getElementById('lesson-p5').innerText= 'সমার্থক শব্দ গুলো';
+    document.getElementById('lesson-heading').innerText = `${lesson.word} ( ${lesson.pronunciation} )`;
+    document.getElementById('lesson-p1').innerText = 'Meaning';
+    document.getElementById('lesson-p2').innerText = lesson.meaning;
+    document.getElementById('lesson-p3').innerText = 'Example';
+    document.getElementById('lesson-p4').innerText = lesson.sentence;
+    document.getElementById('lesson-p5').innerText = 'সমার্থক শব্দ গুলো';
 
     const buttons = lesson.synonyms;
     const buttonContainer = document.getElementById('button-container');
-    buttonContainer.innerHTML= ``;
+    buttonContainer.innerHTML = ``;
 
-    buttons.forEach((button)=>{
-    const eachButton = document.createElement('button');
-    eachButton.classList.add('btn', 'm-2');
-    eachButton.innerText = `${button}`
-    buttonContainer.append(eachButton);
+    buttons.forEach((button) => {
+        const eachButton = document.createElement('button');
+        eachButton.classList.add('btn', 'm-2');
+        eachButton.innerText = `${button}`
+        buttonContainer.append(eachButton);
     });
-    
-
-
-}
-
-const setActiveButton = (clickedButton) => {
-    const allButtons = document.querySelectorAll('#category-buttons button');
-
-    // Remove active styles from all buttons
-    allButtons.forEach(button => {
-        button.classList.remove('bg-[#422AD5]', 'text-white');
-        button.classList.add('text-[#422AD5]', 'border-[#422AD5]');
-    });
-
-    // Add active class to the clicked button
-    clickedButton.classList.add('bg-[#422AD5]', 'text-white');
-    clickedButton.classList.remove('text-[#422AD5]', 'border-[#422AD5]');
 };
-
-
-
 
 const loadCategories = (categoryName) => {
+    showLoader();
     fetch(`https://openapi.programming-hero.com/api/level/${categoryName}`)
         .then(info => info.json())
-        .then(data => showCategories(data.data))
-};
+        .then(data => {
+            removerActiveClass();
+            const clikedBUtton = document.getElementById(`btn-${categoryName}`);
+            clikedBUtton.classList.add('active');
 
+            showCategories(data.data)
+        });
+};
 
 const showCategories = (lessons) => {
 
     const lessonContainer = document.getElementById('details-container');
     lessonContainer.innerHTML = ``;
 
-    if (!lessons || lessons.length === 0) {
+        if (lessons.length < 1) {
         lessonContainer.classList.remove('grid', 'grid-cols-3');
         lessonContainer.innerHTML = `
         <div class="flex justify-center items-center h-[300px]">
         <p class="text-center font-bold text-3xl">No Word Found!!!</p>
     </div>
         `;
+        hideLoader();
         return;
-    }
+    };
 
     lessons.forEach((lesson) => {
         lessonContainer.classList.add('grid', 'grid-cols-3', 'gap-4');
         const div = document.createElement('div');
-        div.classList.add('rounded-md', 'p-10', 'shadow-md', 'bg-white', 'p-10', 'border','border-gray-300');
+        div.classList.add('rounded-md', 'p-10', 'shadow-md', 'bg-white', 'p-10', 'border', 'border-gray-300');
         div.innerHTML = `
         <div class="text-center space-y-10">
                     <p class="font-bold text-3xl">${lesson.word}</p>
@@ -121,14 +129,8 @@ const showCategories = (lessons) => {
                   </div>
         `
         lessonContainer.appendChild(div);
-
-    })
+    });
+  hideLoader();
 };
-
-const lessonDetails =
-
-
-
-
-
-    loadButtons()
+ 
+loadButtons();
